dce91790c79fe44413ad48933d89deaa301a9228
Elves Sousa <elves.sousa@gmail.com>
2020-07-19 19:19:57 -0300

    Single post page.

--- pages/[lang]/post/[id].tsx
+++ pages/[lang]/post/[id].tsx
@@ -1,12 +1,54 @@
-import { NextPage } from "next";
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
+
+import { getAllPostIds, getPostData } from "../../../lib/posts";
 import Layout from "../../../components/Layout";
 
-const Post: NextPage = () => {
+interface Props {
+  locale: string;
+  postData: {
+    lang: string;
+    title: string;
+    slug: string;
+    date: string;
+    category: string;
+    contentHtml: string;
+  };
+}
+
+const Post: NextPage<Props> = ({ postData, locale }) => {
+  const { lang, title, slug, date, category, contentHtml } = postData;
+
   return (
-    <Layout className="post" title="Post">
-      <h1>Title</h1>
+    <Layout title={title}>
+      <article className="post-content">
+        <h1>{title}</h1>
+        <div
+          className="post-text"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </article>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(`/${params.lang}/${params.id}`);
+
+  return {
+    props: {
+      locale: params?.lang || "pt",
+      postData,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export default Post;
