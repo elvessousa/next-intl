a6745e0a11d7960a6b70483fd4bbee6095c78270
Elves Sousa <elves.sousa@gmail.com>
2020-07-19 16:59:13 -0300

    Single post page.

---
+++ pages/[lang]/post/[id].tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Layout from "../../../components/Layout";
+
+const Post: NextPage = () => {
+  return (
+    <Layout className="post" title="Post">
+      <h1>Title</h1>
+    </Layout>
+  );
+};
+
+export default Post;
