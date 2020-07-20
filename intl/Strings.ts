d4f6a0ceeea067b27172591b93e2222cf0dc3534
Elves Sousa <elves.sousa@gmail.com>
2020-07-20 16:19:13 -0300

    Translation strings.

---
+++ intl/Strings.ts
@@ -0,0 +1,14 @@
+export const LangStrings = {
+  en: {
+    about: "About",
+    articles: "Articles",
+    home: "Home",
+    slogan: "An example site showcasing a bilingual site with GatsbyJS.",
+  },
+  pt: {
+    about: "Sobre",
+    articles: "Artigos",
+    home: "Início",
+    slogan: "Um site bilíngue de exemplo feito com GatsbyJS.",
+  },
+};
