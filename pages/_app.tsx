cb7e8fc3ad226b7a9ca7220624f710f57b216a0c
Elves Sousa <elves.sousa@gmail.com>
2020-07-20 16:36:12 -0300

    Importing context on the app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppPropsType } from "next/dist/next-server/lib/utils";
+import { LanguageProvider } from "../intl/LanguageProvider";
 
 import "./styles/layout.css";
 
 export default function App({ Component, pageProps, router }: AppPropsType) {
-  return <Component {...pageProps} key={router.route} />;
+  return (
+    <LanguageProvider>
+      <Component {...pageProps} key={router.route} />
+    </LanguageProvider>
+  );
 }
