55f4768de5cc7280e2b8925a1e0df49d1fd5407f
Elves Sousa <elves.sousa@gmail.com>
2020-07-24 15:41:15 -0300

    Opengraph.

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,27 +1,46 @@
 import React, { useContext } from "react";
-import { Helmet } from "react-helmet";
+import Head from "next/head";
 
 import Header from "../Header";
 import Footer from "../Footer";
-import { LanguageContext } from "../../intl/LanguageProvider";
 
 interface Props {
   className?: string;
   children: React.ReactNode;
+  desc?: string;
   title: string;
+  thumb?: string;
 }
 
-const Layout: React.FC<Props> = ({ className, children, title }) => {
-  const [locale] = useContext(LanguageContext);
+const Layout: React.FC<Props> = ({
+  className,
+  children,
+  title,
+  desc,
+  thumb,
+}) => {
+  const description = desc || "A Next.js multilingual site";
+  const thumbnail =
+    thumb ||
+    "https://repository-images.githubusercontent.com/280748222/db50d880-cac8-11ea-8874-40a3c45a9fa7";
 
   return (
     <main className={className}>
-      <Helmet>
-        <html lang={locale} />
+      <Head>
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <meta name="description" content="A Next.js multilingual site" />
-      </Helmet>
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={thumbnail} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={thumbnail} />
+      </Head>
       <Header />
       {children}
       <Footer>
