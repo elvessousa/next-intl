184b397b87a8492ea97cbefa94cc467a8878694f
Elves Sousa <elves.sousa@gmail.com>
2021-03-09 14:22:05 -0300

    Typo

--- intl/Strings.ts
+++ intl/Strings.ts
@@ -1,14 +1,14 @@
 export const LangStrings = {
   en: {
-    about: "About",
-    articles: "Articles",
-    home: "Home",
-    slogan: "An example site showcasing a bilingual site with GatsbyJS.",
+    about: 'About',
+    articles: 'Articles',
+    home: 'Home',
+    slogan: 'An example site showcasing a bilingual site with Next.js.',
   },
   pt: {
-    about: "Sobre",
-    articles: "Artigos",
-    home: "Início",
-    slogan: "Um site bilíngue de exemplo feito com GatsbyJS.",
+    about: 'Sobre',
+    articles: 'Artigos',
+    home: 'Início',
+    slogan: 'Um site bilíngue de exemplo feito com Next.js.',
   },
 };
